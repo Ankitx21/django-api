7e0c5bcc26fb97b4cae8776c63db166cec1b4391
Ankit Varma <122334471+Ankitx21@users.noreply.github.com>
2025-07-21 12:37:52 +0530

    Update urls.py

--- energy/urls.py
+++ energy/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import energy_predictions, upload_csv
 
 urlpatterns = [
-    path('energy-predictions/', views.energy_predictions, name='energy_predictions'),
-    path('energy/', include('energy.urls')),
+    path('energy-predictions/', energy_predictions, name='energy_predictions'),
+    path('upload-csv/', upload_csv, name='upload_csv'),
 ]
