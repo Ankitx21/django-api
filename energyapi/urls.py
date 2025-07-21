8462609b7aa978650362bc93dc3ab167fd1e9aa3
Ankit <ankit.varma@iihs.ac.in>
2025-07-21 12:02:42 +0530

    Fix URL configuration to include energy app URLs

--- energyapi/urls.py
+++ energyapi/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from energy.views import energy_predictions
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('energy-predictions/', energy_predictions, name='energy_predictions'),
+    path('energy/', include('energy.urls')),
 ]
